c93c487c1945bfc8b132bab4f254ba4b94dc09a2
3102467 <3102467@TE-GDA-D-D36G11>
2024-03-13 12:31:22 +0100

    czolg

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,38 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class LevelManager : MonoBehaviour
 {
-
+    //pozycja gracza
     Transform player;
+
+    //prefab przeciwnika
     public GameObject basherPrefab;
-    float spawnInterval = 1;
+
+    //czas miêdzy respawnem kolejnego bashera
+    public float spawnInterval = 1;
+
+    //czas od ostatniego respawnu
     float timeSinceSpawn;
+
+    //bezpieczna odleg³oœæ spawnu
     float spawnDistance = 30;
+
     // Start is called before the first frame update
     void Start()
     {
+        //zlinkuj aktualna pozycje gracza do zmiennej transform
         player = GameObject.FindWithTag("Player").transform;
+
+        //zerujemy licznik
+        timeSinceSpawn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //dodaj do czasu od ostatniego spawnu czas od ostatniej klatki (ostatni update())
         timeSinceSpawn += Time.deltaTime;
 
-        if(timeSinceSpawn > spawnInterval) {
+        //je¿eli d³u¿ej ni¿ jedna sekunda
+        if(timeSinceSpawn > spawnInterval)
+        {
+            //wygeneruj losow¹ pozycje
             //Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
-            Vector2 random = Random.insideUnitCircle.normalized;
-            Vector3 randomPosition = new Vector3(random.x, 0, random.y);
-            randomPosition *= spawnDistance;
-            randomPosition += player.position;
-            //if (!Physics.CheckSphere(new Vector3(randomPosition.x)));
-            Instantiate(basherPrefab, randomPosition, Quaternion.identity);
-            timeSinceSpawn = 0;
-        }
-        
 
+            //wygeneruj randomow¹ pozycjê na kole o promieniu 1
+            Vector2 random = Random.insideUnitCircle.normalized;
+
+            //skonwertuj x,y na x,z i zerow¹ wysokoœæ
+            Vector3 randomPosition = new Vector3(random.x, 0, random.y);
+
+            //zwielokrotnij odleg³osæ od gracza tak, ¿eby spawn nastêpowa³ poza kamer¹
+            randomPosition *= spawnDistance;
+
+            //dodaj do niej pozycje gracza tak, aby nowe wspó³rzêdne by³y pozycj¹ wzglêdem gracza
+            randomPosition += player.position;
+
+            //sprawdz czy danej miejsce jest wolne
+            if(!Physics.CheckSphere(new Vector3(randomPosition.x, 1, randomPosition.z), 0.5f))
+            {
+                //stworz nowego przeciwnika z istniej¹cego prefaba, na pozycji randomPosition z rotacj¹ domyœln¹
+                Instantiate(basherPrefab, randomPosition, Quaternion.identity);
+
+                //wyzeruj licznik
+                timeSinceSpawn = 0;
+            }
+            //jeœli miejsce bêdzie zajête to program podejmie kolejn¹ próbê w nastêpnej klatce
+            
+        }
+
+        //TODO: opracowaæ sposób na przyspieszanie spawnu w nieskoñczonoœæ wraz z d³ugoœcia trwania etapu
+
+        
     }
 }
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,20 +1,34 @@
-ï»¿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    //zasiç’• broni
+    //zasiêg broni
     public float range = 10f;
 
     //transform gracza
     Transform player;
+
+    //prefab pocisku
+    public GameObject projectilePrefab;
+
+    //spawn pocisku
+    Transform projectileSpawn;
+
+    //czestotliwosc strzalu (/sek)
+    public float rateOfFire = 1;
+    //czas od ostatniego wystrzalu
+    float timeSinceLastFire = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         // pozycja gracza
         player = GameObject.FindWithTag("Player").transform;
+
+        //znajdz w hierarchii obieku miejsce z ktorego staruje pocisk
+        projectileSpawn = transform.Find("ProjectileSpawn").transform;
     }
 
     // Update is called once per frame
@@ -25,25 +39,39 @@ public class WeaponController : MonoBehaviour
         {
             Debug.Log("Celuje do: " + target.gameObject.name);
             transform.LookAt(target.position + Vector3.up);
+
+            //wystrzel pocisk
+            //jeœli minê³o wiêcej od ostatniego strza³u ni¿ wskazuje na to prêdkoœæ strzelania
+            if (timeSinceLastFire > rateOfFire)
+            {
+                Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity);
+                //je¿eli strzelisz to wyzeruj czas 
+                timeSinceLastFire = 0;
+            }
+            else
+            {
+                timeSinceLastFire += Time.deltaTime;
+            }
         }
+
     }
     Transform TagTargeter(string tag)
     {
-   
+        //tablica wszystkich obiektów pasuj¹cych do taga podanego jako agument
         GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
 
-    
+        //szukamy najbli¿szego
         Transform closestTarget = transform;
         float closestDistance = Mathf.Infinity;
 
         foreach (GameObject target in targets)
         {
-          
+            //wektor przesuniêcia wzglêdem gracza
             Vector3 difference = target.transform.position - player.position;
-       
+            //odleg³oœæ od gracza
             float distance = difference.magnitude;
 
-            if (distance < closestDistance && distance < range)
+            if (distance < closestDistance && distance < range) 
             {
                 closestTarget = target.transform;
                 closestDistance = distance;
@@ -54,34 +82,39 @@ public class WeaponController : MonoBehaviour
 
     Transform LegeacyTargeter()
     {
-      
+        //znajdz wszystkie colidery w promieniu = range i zapisz je do tablicy collidersInRange
         Collider[] collidersInRange = Physics.OverlapSphere(transform.position, range);
 
-       
+        //do celów testowych 
+        //Debug.Log("Iloœæ colliderów w zasiêgu broni: " +  collidersInRange.Length);
+
+        //szukamy najbli¿szego przeciwnika
 
         Transform target = transform;
         float targetDistance = Mathf.Infinity;
 
         foreach (Collider collider in collidersInRange)
         {
-            
+            //wyci¹gnij transforma od tego coldiera
+
+            //najpierw znajdz kapsu³e/model (w³aœciciela colidera)
             GameObject model = collider.gameObject;
 
             if (model.transform.parent != null)
             {
-               
+                //znajdz rodzica modelu czyli przeciwnika
                 GameObject enemy = model.transform.parent.gameObject;
 
-              
+                //sprawdz czy to co znalaz³eœ jest przeciwnikiem
                 if (enemy.CompareTag("Enemy"))
                 {
-                  
+                    //jeœli to przeciwnik to okreœl wektor przesuniêcia
                     Vector3 diference = player.position - enemy.transform.position;
-                  
+                    //policz d³ugoœæ wektora (odleg³oœæ)
                     float distance = diference.magnitude;
                     if (distance < targetDistance)
                     {
-                      
+                        //znaleziono nowy cel bli¿ej
                         target = enemy.transform;
                         targetDistance = distance;
                     }
@@ -91,7 +124,7 @@ public class WeaponController : MonoBehaviour
 
         }
 
-
+        //do celów testowych
         Debug.Log("Celuje do: " + target.gameObject.name);
 
         return target;
